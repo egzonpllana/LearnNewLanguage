5049052b32bf924d15be064955972949e4b3d14c
Egzon <docpllana@gmail.com>
2020-08-16 14:51:23 +0200

    Create project from XCode.

---
+++ FallingWords/FallingWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FallingWords
+//
+//  Created by Egzon Pllana on 8/16/20.
+//  Copyright © 2020 Native Coders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
